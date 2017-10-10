9eeb99a824d085ac443062630ef71398c3e46f9e
Patrick Modafferi <pat.modafferi@gmail.com>
2017-10-10 18:43:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function nowServing(katzDeliLine){
 
 function currentLine(katzDeliLine) {
   var message = 'The line is currently: '
-  if (katzDeliLine.length === 0) {
+  if (katzDeliLine.length == 0) {
     return 'The line is currently empty.'
   } else {
     for (var i = 0; i < katzDeliLine.length; i++) {
