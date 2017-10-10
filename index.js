e15cfe3b274d4d7233a59f65f0e4939a4fd09606
Patrick Modafferi <pat.modafferi@gmail.com>
2017-10-10 18:37:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   return 'Welcome, '+ name +'. You are number ' + katzDeliLine.length + ' in line.'
 }
+
+function nowServing(katzDeliLine){
+  var name = katzDeliLine.shift()
+  if (name === undefined) {
+    return 'There is nobody waiting to be served!'
+  } else {
+    return 'Currently serving ' + name + '.'
+  }
+}
