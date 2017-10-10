c1cffdf84de73eba3af312b9fbb64dfade1a4f50
Patrick Modafferi <pat.modafferi@gmail.com>
2017-10-10 18:23:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
+function nowServing(array) {
+if (array.length === 0) {
+  return `There is nobody waiting to be served!`
+}
+}
