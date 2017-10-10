9d7731533a6069c65059120bb7848af08a2a6d86
Patrick Modafferi <pat.modafferi@gmail.com>
2017-10-10 18:35:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-var katzDeli = [];
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
-}
-function nowServing(array) {
-if (array.length === 0) {
-  return "There is nobody waiting to be served!"
-}
-else {
-  return `Currently serving ${array.shift()}`
-}
+  return 'Welcome, '+ name +'. You are number ' + katzDeliLine.length + ' in line.'
 }
